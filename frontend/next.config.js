f4edfb7a351065551502c74fec8911c64378b2fe
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-10-09 18:56:53 +0530

    try deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require("path");
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
