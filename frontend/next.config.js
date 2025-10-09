8bf3b94ec747c1030800f9a7b955a13a6bfd77ec
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-10-09 19:10:52 +0530

    release

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"),
-    };
-    return config;
-  },
+  // Use tsconfig.json paths; no manual webpack alias needed
 };
 
 module.exports = nextConfig;
