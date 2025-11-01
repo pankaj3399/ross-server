8008834f8bde6602741a15f4234eac0e1b8f80fc
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-11-01 23:30:05 +0530

    Stub fix

---
+++ backend/migrations/1761469686760_add-score-table.js
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  // This migration was already run in the database.
+  // This is a stub file to maintain migration history.
+  // If you need to recreate the score table, add the CREATE TABLE statement here.
+};
+
+exports.down = (pgm) => {
+  // Stub for rollback - already applied migration
+};
