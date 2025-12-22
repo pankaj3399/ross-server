04dd034c516d3827fe9d159ba5559e2f2b29dec6
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-12-22 13:04:42 +0530

    Update page title

--- landing-page/src/components/HeroImage.jsx
+++ landing-page/src/components/HeroImage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const HeroImage = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <img 
-        src='https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public' 
-        alt='Hostinger Horizons' 
+    <div className="flex justify-center items-center">
+      <img
+        src="https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public"
+        alt="MATUR.ai - AI Maturity Assessment Platform"
       />
     </div>
   );
